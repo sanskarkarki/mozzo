6085d5cf82054e6e1009fe39c5af04439cba1672
sanskarkarki <68042549+sanskarkarki@users.noreply.github.com>
2026-05-05 17:58:23 +0545

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ async function placeOrder() {
   message += `\nTotal: NPR ${total}`;
 
   const BOT_TOKEN = "8721297737:AAHIFGFLK3gOX_T2Yq3_KWgA6csvt1Nykts";
-  const CHAT_ID = "1126605588"; // ⚠️ REPLACE THIS
+  const CHAT_ID = "847217259"; // ⚠️ REPLACE THIS
 
   try {
     await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
